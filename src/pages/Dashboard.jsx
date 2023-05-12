a68b47e44c4dd36cce8cc1c6748ac456bd438885
Ayushi Saini <101177651+SainiAyushi@users.noreply.github.com>
2023-05-12 15:05:14 +0530

    Added Expense Tables

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -8,6 +8,8 @@ import { toast } from "react-toastify";
 import Intro from "../components/Intro";
 import AddBudgetForm from "../components/AddBudgetForm";
 import AddExpenseForm from "../components/AddExpenseForm";
+import BudgetItem from "../components/BudgetItem";
+import Table from "../components/Table";
 
 //  helper functions
 import { createBudget, createExpense, fetchData, waait } from "../helpers"
@@ -16,7 +18,8 @@ import { createBudget, createExpense, fetchData, waait } from "../helpers"
 export function dashboardLoader() {
   const userName = fetchData("userName");
   const budgets = fetchData("budgets");
-  return { userName, budgets }
+  const expenses = fetchData("expenses");
+  return { userName, budgets, expenses }
 }
 
 // action
@@ -63,7 +66,7 @@ export async function dashboardAction({ request }) {
 }
 
 const Dashboard = () => {
-  const { userName, budgets } = useLoaderData()
+  const { userName, budgets, expenses } = useLoaderData()
 
   return (
     <>
@@ -79,6 +82,22 @@ const Dashboard = () => {
                       <AddBudgetForm />
                       <AddExpenseForm budgets={budgets} />
                     </div>
+                    <h2>Existing Budgets</h2>
+                    <div className="budgets">
+                      {
+                        budgets.map((budget) => (
+                          <BudgetItem key={budget.id} budget={budget} />
+                        ))
+                      }
+                    </div>
+                    {
+                      expenses && expenses.length > 0 && (
+                        <div className="grid-md">
+                          <h2>Recent Expenses</h2>
+                          <Table expenses={expenses.sort((a, b) => b.createdAt - a.createdAt)} />
+                        </div>
+                      )
+                    }
                   </div>
                 )
                 : (
